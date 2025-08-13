3b342e8a56a7b9f3ea33ef0d73e3a895e34280d9
SamuelMuniz9 <msamuelmuniz@gmail.com>
2025-08-13 16:42:43 -0300

    leet 326. Power of Three

--- Easy/Power_of_Three.py
+++ Easy/Power_of_Three.py
@@ -10,12 +10,29 @@ Input: n = 27
 Output: True
 Explicação: 27 = 3^3
 
-Abordagem:
+Abordagem: Fiz um loop para gerar potências de 3 começando de 3^0.
+A cada iteração, comparamos o resultado com n:
+- Se for igual, retornamos True.
+- Se a potência gerada ultrapassar n, encerramos o loop e retornamos False.
+Essa abordagem evita cálculos desnecessários e não requer estruturas extras.
 
 
 Complexididade:  
-Tempo:
-Espaço: 
+Tempo: O(log₃ n) –> a cada iteração multiplicamos a potência por 3, logo o número de passos cresce proporcionalmente ao logaritmo na base 3 de n.
+Espaço: O(1) –> usamos apenas variáveis simples, sem estruturas adicionais.
 """
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        x = 0
+        while 3 ** x <= n:
+            if n == 3 ** x:
+                return True
+            x += 1
+        return False
 
-
+# Exemplo de uso fora do LeetCode
+if __name__ == "__main__":
+    sol = Solution()    
+    print(sol.isPowerOfThree(9))  #  Retornando True
+    print()
+    print(sol.isPowerOfThree(2))  #  Retornando False
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # LeetCode Solutions
 Soluções organizadas por dificuldade e comentadas.
+
 Inicialmente optei por realizar em Python os problemas. 
 Começado no dia 13/08/25
 
