149561d753120cc91482eda841221b993888d9d5
SamuelMuniz9 <msamuelmuniz@gmail.com>
2025-08-13 11:13:21 -0300

    Primeiro_Commit

---
+++ Easy/Power_of_Three.py
@@ -0,0 +1,21 @@
+"""
+LeetCode 326 – Power of Three
+
+Enunciado:
+Dado um inteiro n, retorne True se for potência de 3, caso contrário False.
+Uma potência de 3 é um número da forma 3^x onde x é um inteiro não-negativo.
+
+Exemplo:
+Input: n = 27
+Output: True
+Explicação: 27 = 3^3
+
+Abordagem:
+
+
+Complexididade:  
+Tempo:
+Espaço: 
+"""
+
+
