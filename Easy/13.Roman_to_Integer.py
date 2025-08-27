378a764aa5d2cb6a6e7a9d5ed7e3a4048ffaa5f8
SamuelMuniz9 <msamuelmuniz@gmail.com>
2025-08-27 11:16:33 -0300

    13.Roman_to_Integer

--- Easy/13.Roman_to_Integer.py
+++ Easy/13.Roman_to_Integer.py
@@ -1,65 +1,76 @@
 """
-LeetCode 13 - Roman to Integer
+    LeetCode 13 - Roman to Integer
 
-Enunciado:
-Converter um número romano (string) para um número inteiro.
+    Enunciado:
+    Converter um número romano (string) para um número inteiro.
 
-Exemplo:
+    Exemplo:
 
 
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
 
-Example 1:
+    Example 1:
 
-Input: s = "III"
-Output: 3
-Explanation: III = 3.
-Example 2:
+    Input: s = "III"
+    Output: 3
+    Explanation: III = 3.
+    Example 2:
 
-Input: s = "LVIII"
-Output: 58
-Explanation: L = 50, V= 5, III = 3.
-Example 3:
+    Input: s = "LVIII"
+    Output: 58
+    Explanation: L = 50, V= 5, III = 3.
+    Example 3:
 
-Input: s = "MCMXCIV"
-Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- 
+    Input: s = "MCMXCIV"
+    Output: 1994
+    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+    
 
-Abordagem: 
+    Abordagem:
+    - Primeiro, verificar os "combos" (subtrações especiais: IV=4, IX=9, etc.).
+    - Remover esses combos da string e adicionar seus valores diretamente ao total.
+    - Depois, somar os valores restantes dos símbolos romanos.
 
-Complexididade:   
-Tempo: 
-Espaço:
-"""
-S = "IIIX"
+    Complexidade:
+    - Tempo: O(n), onde n é o tamanho da string `S`, pois percorremos cada caractere uma vez.
+    - Espaço: O(1), já que usamos apenas dicionários fixos de valores.
+
+    """
+S = "IIIV"
 valores = {
-"I":1,
-"V": 5,
-"X": 10,
-"L": 50,
-"C": 100,
-"D": 500,
-"M": 1000
-    }
+    "I":1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+        }
+combo_numero = {
+    "IV":4,
+    "IX":9
+}
 
 
 total = 0
+for indice, valor in combo_numero.items():
+    if indice in S: #verifica se possui algum combo)numero no texto
+        total += valor
+        S = S.replace(indice, "")
+    
+    
+
 for letra in S:
-    total += valores[letra]
+        total += valores[letra]
 
 
 print(total)
 
 
-
-#pensando como fazer para reconhecer a logica do x e i e tal, a logica que seria mais facil
-#transformar em numero depois um if, se o maior for o que esta DEPOIS do menor, vc faz uma subtraçao de - o valor do indice da frente, trabalha com array basicamente
-    
